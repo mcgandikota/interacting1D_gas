a3eaf7753cef43fe68360f9774ae2819da045098
mcgandikota <mcgandikota@gmail.com>
2024-06-09 21:13:37 +0530

    Corrected energy calculation, removed diffusion constant definition as it is an overspecification

--- gas.cpp
+++ gas.cpp
@@ -13,19 +13,19 @@ void energy(void);
 float x[20000];
 float velocity[20000];
 float force[20000];
-float deltaT=0.000001;
+float deltaT=0.00001;
 int N;
 float J=1.0;
 float k;
 int signK;
-int skip=100000;
-int print=10000;
-float D=0.500;
+//int skip=3000000;
+int skip=400000;
+int print=1000;
 float Gamma=1; //viscosity 
-float T=1; //temperature
-float bin_size=1.;
-int Nbins=10000;  //even number only
-int bin[20000];
+float T=0.50; //temperature
+float bin_size=0.4;
+int Nbins=100000;  //even number only
+int bin[20000000];
 float potential_energy;
 float harmonic_energy;
 float interaction_energy;
@@ -33,7 +33,7 @@ float kinetic_energy;
 
 random_device rd;
 default_random_engine generator;
-normal_distribution<double> distribution(0.0,2*Gamma*T);
+normal_distribution<double> distribution(0.0,sqrt(Gamma*T)); //coefficient??????????????, you are assuming equilibrium here
 
 int main(int argc, char *argv[]){
 if (argc!=4) {printf("./a.out k N steps\n");exit(0);}
@@ -54,30 +54,32 @@ fclose(energy_out);
 initialize(N);
 generator.seed( rd() );
 
+	cout<<"Equilibration start"<<endl;
 	for (int i=0; i<skip; i++){
 	mdrun();
 	}
+	cout<<"Equilibration end"<<endl;
 
 	for(int i=0; i<steps; i++){
 	mdrun();
 		if(i%print==0){
-			cout<<i<<" ";
-			//print movie
-			movie=fopen("out.dump","a");
-			fprintf(movie,"%d\n\n",N);
-			fclose(movie);
+		cout<<i<<" ";
+		//print movie
+		movie=fopen("out.dump","a");
+		fprintf(movie,"%d\n\n",N);
+		fclose(movie);
 			for(int j=0; j<N; j++){
 			movie=fopen("out.dump","a");
 			fprintf(movie,"1 %f 0.0 0.0\n",x[j]);
 			fclose(movie);
-
-			//print energies
-			energy();
-			energy_out=fopen("energy.dat","a"); //rewwrites file
-			fprintf(energy_out,"%f %f %f\n",potential_energy,kinetic_energy,potential_energy+kinetic_energy);
-			fclose(energy_out);
-
 			}
+
+		//print energies
+		energy();
+		energy_out=fopen("energy.dat","a"); //rewwrites file
+		fprintf(energy_out,"%f %f %f\n",potential_energy,kinetic_energy,potential_energy+kinetic_energy);
+		fclose(energy_out);
+		
 		histogram();
 		}
 	}
@@ -92,20 +94,21 @@ fclose(out);
 
 	for(int i=0; i<(int)Nbins/2; i++){
 	out=fopen("histogram.dat","a");
-	fprintf(out,"%f %f \n",i*bin_size,bin[i]*1.0/(steps/print));
+	fprintf(out,"%f %f \n",i*bin_size/N,bin[i]*1.0/(steps/print));
 	fclose(out);
 	}
 	for(int i=(int)Nbins/2; i<Nbins; i++){
 	out=fopen("histogram.dat","a");
-	fprintf(out,"%f %f \n",-(i-Nbins/2.+1)*bin_size,bin[i]*1.0/(steps/print));
+	fprintf(out,"%f %f \n",-(i-Nbins/2.+1)*bin_size/N,bin[i]*1.0/(steps/print));
 	fclose(out);
 	}
 		
+cout<<endl;
 return 0;
 }
 
 void initialize(int N){
-float space=10;
+float space=0.3;
 
 	for (int i=0; i<N; i++){
 	x[i] = -N/2 + 0.5*space + i*space;  //for even number N this gives a symmetric distribution about x=0
@@ -123,10 +126,9 @@ void mdrun(void){
 
 	for (int i=0; i<N; i++){
 	random=distribution(generator);
-	random=random/sqrt(deltaT);  
-	random=random*sqrt(2*D);//coefficient??????????????
-	x[i] = x[i] + (force[i]+random)*deltaT;
 	velocity[i]=force[i]+random;
+	random=random/sqrt(deltaT);  
+	x[i] = x[i] + (force[i]+random)*deltaT;
 	}
 
 }
@@ -139,7 +141,7 @@ void forces(void){
 
 	for (i=0; i<N; i++){
 	harmonic = -x[i];
-	interactions = 0;
+	interactions = 0.;
 	
 		for (j=0;j<N;j++){
 			if(j!=i){
@@ -147,9 +149,7 @@ void forces(void){
 			}
 		}
 	interactions *= 1./2.*J*signK*k; //need -1 for k<0 and 1 for k>0
-	//cout<<interactions<<endl;
 	force[i]=harmonic+interactions;
-	//cout<<force[i]<<endl;
 	if(force[i]!=force[i]) {cout<<"Blew up\n"<<endl; exit(0);}
 	}
 }
