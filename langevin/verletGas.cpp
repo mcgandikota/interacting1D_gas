72ff7f8cbd982e988ea4302da6607fccacb5c057
mcgandikota <mcgandikota@gmail.com>
2024-06-19 19:53:22 +0530

    Calculating probability density now instead of probability

--- langevin/verletGas.cpp
+++ langevin/verletGas.cpp
@@ -10,21 +10,23 @@ void forces(void);
 void histogram(void);
 void energy(void);
 
-float x[20000];
+float x[20000],xold[20000],xnew[20000];
+float xOld[20000];
 float velocity[20000];
 float force[20000];
-float deltaT=0.00002;
+float deltaT=0.00004;
+float delta2T=deltaT*deltaT;
 int N;
 float J=1.0;
 float k;
 int signK;
 //int skip=3000000;
-int skip=400000;
+int skip=900000;
 int print=10000;
-float Gamma=1; //viscosity 
+float Gamma=10; //viscosity 
 float T=0.50; //temperature
-float bin_size=0.4;
-int Nbins=10000;  //even number only
+float bin_size=0.1;
+int Nbins=1000;  //even number only
 int bin[20000000];
 float potential_energy;
 float harmonic_energy;
@@ -33,8 +35,7 @@ float kinetic_energy;
 
 random_device rd;
 default_random_engine generator;
-normal_distribution<double> distribution(0.0,sqrt(2*T/Gamma)); //coefficient??????????????, you are assuming equilibrium here
-							     //Shouldnt it be sqrt(D) = sqrt(kT/gamma)?
+normal_distribution<double> distribution(0.0,sqrt(2*Gamma*T)); //coefficient??????????????, you are assuming equilibrium here
 
 int main(int argc, char *argv[]){
 if (argc!=4) {printf("./a.out k N steps\n");exit(0);}
@@ -95,7 +96,7 @@ fclose(out);
 
 	for(int i=0; i<(int)Nbins/2; i++){
 	out=fopen("histogram.dat","a");
-	fprintf(out,"%f %f \n",i*bin_size,bin[i]*1.0/(steps/print)/N/bin_size); //probability density = bin_probability/width of bin
+	fprintf(out,"%f %f \n",i*bin_size,bin[i]*1.0/(steps/print)/N/bin_size);
 	fclose(out);
 	}
 	for(int i=(int)Nbins/2; i<Nbins; i++){
@@ -110,9 +111,15 @@ return 0;
 
 void initialize(int N){
 float space=1.0;
+float random;
 
 	for (int i=0; i<N; i++){
 	x[i] = -N/2 + 0.5*space + i*space;  //for even number N this gives a symmetric distribution about x=0
+	xold[i]=x[i];
+	xnew[i]=x[i];
+
+	random=distribution(generator);  //is this correct variance?
+	velocity[i]=random;
 	}
 
 
@@ -127,9 +134,11 @@ void mdrun(void){
 
 	for (int i=0; i<N; i++){
 	random=distribution(generator);
-	velocity[i]=force[i]+random;
-	random=random/sqrt(deltaT);  
-	x[i] = x[i] + (force[i]+random)*deltaT;
+	random=random/sqrt(deltaT);   //Is this correct?
+	xnew[i] = 2*x[i] - xold[i] + (force[i]-Gamma*velocity[i]+random)*delta2T;
+	velocity[i]=(xnew[i]-xold[i])/(2*deltaT);
+	xold[i]=x[i];
+	x[i]=xnew[i];
 	}
 
 }
@@ -140,7 +149,6 @@ float harmonic;
 float interactions; 
 int i,j;
 
-here:
 	for (i=0; i<N; i++){
 	harmonic = -x[i];
 	interactions = 0.;
@@ -152,7 +160,7 @@ here:
 		}
 	interactions *= 1./2.*J*signK*k; //need -1 for k<0 and 1 for k>0
 	force[i]=harmonic+interactions;
-	if(force[i]!=force[i]) {cout<<"Blew up\n"<<endl; goto here;}
+	if(force[i]!=force[i]) {cout<<"Blew up\n"<<endl; exit(0);}
 	}
 }
 
